dadf67354adbef31715456188f390992d3623930
auraPasmin <1288@holbertonschool.com>
2019-12-10 23:17:07 -0500

    kp

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
+for i in range(10):
+    for j in range(10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
         else:
-            print("{}{}".format(i, j), end=", ")
+            if j > i and i is not 8:
+                print("{}{}".format(i, j), end=", ")
