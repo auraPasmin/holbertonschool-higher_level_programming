f512fc875b56b81430617ab3be4cdd0972da82db
auraPasmin <1288@holbertonschool.com>
2020-01-09 21:59:27 -0500

    kp

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Square with a private instance size"""
+    def __init__(self, size=0):
+        self.__size = size
