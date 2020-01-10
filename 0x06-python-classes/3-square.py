b6710e65a97c558ac3c443f1a08da4c865924e0e
auraPasmin <1288@holbertonschool.com>
2020-01-09 22:22:12 -0500

    kp

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,14 +3,14 @@
 
 
 class Square:
-    """Square with a private instance size"""
-        def __init__(self, size=0):
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
+    """Square class"""
+    def __init__(self, size=0):
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if(size < 0):
+            raise ValueError("size must be >= 0")
             self.__size = size
 
-        def area(self):
-            self.area = self.__size * self.__size
-            return self.area
+    def area(self):
+        """Return area"""
+        return self.__size * self.__size
