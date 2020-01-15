2d63f0b74202eee7c1838bfff779b63e893bfa6a
auraPasmin <1288@holbertonschool.com>
2020-01-15 16:27:08 -0500

    kp

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""LockedClass"""
+
+
+class LockedClass:
+    def __init__(self, first_name="", last_name=""):
+        self.first_name = first_name
+        self.__last_name = last_name
