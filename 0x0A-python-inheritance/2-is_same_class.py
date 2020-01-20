1f4dbe7484c74efdd5a7182c5d4cffb7b9a47299
auraPasmin <1288@holbertonschool.com>
2020-01-20 16:18:59 -0500

    kp

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" True if the object is exactly an instance
+of the specified class ; otherwise False.
+"""
+
+
+def is_same_class(obj, a_class):
+
+    """pass"""
+    return True if type(obj) == a_class else False
