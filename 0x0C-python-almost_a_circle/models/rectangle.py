f532791e3af805923da1821d8b65d97eac7b0958
auraPasmin <1288@holbertonschool.com>
2020-01-27 13:25:40 -0500

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """initializer builder"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """first part """
         super().__init__(id)
         self.width = width
         self.height = height
