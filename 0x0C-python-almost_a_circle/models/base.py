e092d6d99b638ea7747fddf725ddfe5100c970f2
auraPasmin <1288@holbertonschool.com>
2020-01-27 15:37:02 -0500

    kp

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,46 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """epresentation of list_dictionaries:"""
+        """representation of list_dictionaries:"""
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+        @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs"""
+        file = cls.__name__ + ".json"
+        empty = []
+        if list_objs:
+            for i in list_objs:
+                empty.append(cls.to_dictionary(i))
+        with open(file, "w") as w:
+            w.write(cls.to_json_string(empty))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance"""
+        if cls.__name__ is "Rectangle":
+            from models.rectangle import Rectangle
+            obj = Rectangle(1, 1)
+        elif cls.__name__ is "Square":
+            from models.square import Square
+            obj = Square(1)
+        obj.update(**dictionary)
+        return obj
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        file = cls.__name__ + ".json"
+        if not path.isfile(file):
+            return []
+        with open(file, "r", encoding="utf-8") as f:
+            return [cls.create(**d) for d in cls.from_json_string(f.read())]
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
