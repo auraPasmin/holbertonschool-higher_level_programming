3b314bddcb5a4203014ce7af9b27c831ed7e0d35
auraPasmin <1288@holbertonschool.com>
2020-01-27 15:44:49 -0500

    kp

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,22 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """function"""
+        file = cls.__name__+".json"
+        list = []
+        if list_objs:
+            for i in list_objs:
+                list.append(cls.to_dictionary(i))
+        with open(file, "w") as f:
+            f.write(cls.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """the json to string"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
