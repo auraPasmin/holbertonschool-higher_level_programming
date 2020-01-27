8440c499c4a5475898a2e336bffcb0ab23e773f2
auraPasmin <1288@holbertonschool.com>
2020-01-27 12:48:16 -0500

    fix base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ class base """
 
+
 class base:
+    """ class """
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """ class initializer"""
         if id is not None:
             self.id = id
         else:
             type(self).__nb_objects += 1
-            self.id =type(self).__nb_objects
-    
-    
-
+            self.id = type(self).__nb_objects
