59dd35ecabcaac5b46e16e5bb8f9c5e4be1cee85
auraPasmin <1288@holbertonschool.com>
2020-01-27 15:32:45 -0500

    fix base 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ class base """
+import json
+from os import path
 
 
 class Base:
@@ -13,3 +15,10 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """epresentation of list_dictionaries:"""
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
