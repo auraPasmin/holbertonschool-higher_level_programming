a5ac551e648ec7d0390da7477315f98958d14d82
auraPasmin <1288@holbertonschool.com>
2020-01-27 13:38:30 -0500

    kp

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """initializer builder"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """first part """
         super().__init__(id)
@@ -33,3 +34,39 @@ class Rectangle(Base):
     def y(self):
         """Getter and Setter"""
         return self.__y
+
+    @width.setter
+    def width(self, value):
+        """setter function"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """setter function"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """setter function"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """setter function"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
