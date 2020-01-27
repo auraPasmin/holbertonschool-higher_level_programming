4aa8c0612b2400a4fad02748282245677a16078f
auraPasmin <1288@holbertonschool.com>
2020-01-27 15:07:55 -0500

    fix squared

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """builder"""
+        super().__init__(size, size, x, y, id)
