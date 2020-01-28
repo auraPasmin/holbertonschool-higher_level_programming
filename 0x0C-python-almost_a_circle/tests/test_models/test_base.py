bf90140f195f089d4288be43b54b22c410ddd014
auraPasmin <1288@holbertonschool.com>
2020-01-27 21:49:19 -0500

    fix tb

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Unittest base.py"""
+
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import unittest
+import json
+import pep8
+import unittest
+
+class TestBase(unittest.TestCase):
+    """Base.py tests"""
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_0(self):
+        """Test number 0 for base"""
+	base0 = Base()
+	base1 = Base()
+	base2 = Base()
+	self.assertEqual(base0.id, 1)
+	self.assertEqual(base1.id, 2)
+	self.assertEqual(base2.id, 3)
