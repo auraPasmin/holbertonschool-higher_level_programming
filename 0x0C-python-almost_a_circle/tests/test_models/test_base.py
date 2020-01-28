0aef5464d653ab2a0c8579de640de8946398682d
auraPasmin <1288@holbertonschool.com>
2020-01-27 21:53:01 -0500

    fix base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,6 +9,7 @@ import json
 import pep8
 import unittest
 
+
 class TestBase(unittest.TestCase):
     """Base.py tests"""
 
@@ -17,9 +18,9 @@ class TestBase(unittest.TestCase):
 
     def test_0(self):
         """Test number 0 for base"""
-	base0 = Base()
-	base1 = Base()
-	base2 = Base()
-	self.assertEqual(base0.id, 1)
-	self.assertEqual(base1.id, 2)
-	self.assertEqual(base2.id, 3)
+        base0 = Base()
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base0.id, 1)
+        self.assertEqual(base1.id, 2)
+        self.assertEqual(base2.id, 3)
