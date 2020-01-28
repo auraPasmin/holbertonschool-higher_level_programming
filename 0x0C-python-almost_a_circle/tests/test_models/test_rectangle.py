a747d8e5a56d2a1765dbe8e9a09c363eecab3502
auraPasmin <1288@holbertonschool.com>
2020-01-27 22:24:25 -0500

    fix re

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Unittest for rectangle"""
+
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class RectangleTest(unittest.TestCase):
+    """Tests for rectangle class."""
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_0(self):
+        """Test 0 for rectangle"""
+        Rect = Rectangle(1, 2)
+        self.assertEqual(isinstance(Rect, Base), True)
