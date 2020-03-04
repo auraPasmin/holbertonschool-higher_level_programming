ef6b7133b1c04786ace9556e508ed25895f57f9f
auraPasmin <1288@holbertonschool.com>
2020-03-03 22:49:21 -0500

    fix 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,7 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres, tv_genres WHERE tv_show_genres.genre_id = tv_genres.id AND tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
-© 2020 GitHub, Inc.
+-- join
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows 
+INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
