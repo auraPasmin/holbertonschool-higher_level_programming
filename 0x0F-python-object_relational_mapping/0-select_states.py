25b4b8520389218dca5fdc159364fc64557b8b4f
auraPasmin <1288@holbertonschool.com>
2020-03-28 18:35:24 -0500

    add 0 py and 0-sql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+# connect to mysql and list table state
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    # arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # connect to database
+    ct_db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            ct_db=database,
+            port=3306
+    )
+
+    cursor = ct_db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    records = cursor.fetchall()
+    for row in records:
+        print(row)
+    cursor.close()
+    ct_db.close()
