915f76286ec0194a95f5e4989051318e1a1f1d13
auraPasmin <1288@holbertonschool.com>
2020-03-28 21:04:34 -0500

    add 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+and return matching states; safe from MySQL injections.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    """Connect to database."""
+    ct_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
+    """Create cursor to exec queries using SQL."""
+    cursor = ct_db.cursor()
+    search_name = """
+    SELECT * FROM states
+    WHERE BINARY name=%s
+    ORDER BY id ASC"""
+
+    cursor.execute(search_name, (argv[4],))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    ct_db.close()
