1f155a54d639e6b128ecb40279391c75908b646a
auraPasmin <1288@holbertonschool.com>
2020-03-29 16:34:47 -0500

    add 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+    Script that lists all State objects from the database hbtn_0e_6_usa.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    """from the database."""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+            pool_pre_ping=True
+        )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    records = session.query(State).order_by(State.id).all()
+    for row in records:
+        print("{}: {}".format(row.id, row.name))
+    session.close()
