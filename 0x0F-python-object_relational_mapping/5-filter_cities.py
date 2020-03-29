3bc00940c1175be98c6d5c60cf8be3b85be778fa
auraPasmin <1288@holbertonschool.com>
2020-03-28 21:26:48 -0500

    add 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""
+Write a script that takes in the name of a state as
+ian argument and lists all cities o that state, using the database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    """Connect to database."""
+    ct_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
+    """Create cursor to exec queries using SQL."""
+    cursor = ct_db.cursor()
+    search_name = "SELECT cities.name\
+                FROM cities\
+                LEFT JOIN states\
+                ON cities.state_id = states.id\
+                ORDER BY cities.id ASC"
+
+    cursor.execute(search_name, (name_inp,))
+    separator = ""
+    for row in cursor.fetchall():
+        print("{}{}".format(separator, row[0]), end="")
+        separator = ", "
+    print("")
+    cursor.close()
+    ct_db.close()
