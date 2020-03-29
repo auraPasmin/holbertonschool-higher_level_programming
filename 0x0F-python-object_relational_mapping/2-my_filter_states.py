e93d862e6ee9c51751817e5b211cd5afc59d1215
auraPasmin <1288@holbertonschool.com>
2020-03-28 20:58:49 -0500

    add 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+    Script that takes in an argument and displays all values
+    in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    """Connect to database."""
+    ct_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
+    """Create cursor to exec queries using SQL."""
+    cursor = ct_db.cursor()
+    search_name = """
+    SELECT * FROM states
+    WHERE name LIKE BINARY '{:s}'
+    ORDER BY id ASC""".format(argv[4])
+
+    cursor.execute(search_name)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    ct_db.close()
