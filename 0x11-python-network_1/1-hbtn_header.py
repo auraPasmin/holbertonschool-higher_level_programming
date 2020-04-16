cfa35df13c449a23da1317e195d2d8f00756ec2d
auraPasmin <1288@holbertonschool.com>
2020-04-15 23:31:56 -0500

    add 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""value of the X-Request-Id variable found in the header of the response."""
+from urllib.request import urlopen
+import sys
+
+if __name__ == "__main__":
+    url = urllib.request.Request(argv[1])
+    with urlopen(url) as response:
+        header = response.getheader("X-Request-Id")
+    print(header)
