0e45460ab26d1523585ba533a7c871073ac3c89f
auraPasmin <1288@holbertonschool.com>
2020-04-16 17:35:39 -0500

    add 10 permissions ok

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" """
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(res.json().get('id'))
