e097b4c5e094635dcd3efb841ff74a487375b092
auraPasmin <1288@holbertonschool.com>
2020-04-16 16:20:00 -0500

    add 5 permission ok

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Write a Python script that takes in a URL """
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    p = requests.get(p)
+    print(p.headers.get('X-Request-Id'))
