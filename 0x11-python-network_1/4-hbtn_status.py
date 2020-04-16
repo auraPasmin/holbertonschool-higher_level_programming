7d5f7433c3d269e7e65702de8c81e34e303d2036
auraPasmin <1288@holbertonschool.com>
2020-04-16 16:10:39 -0500

    add 4 permissions ok

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""  """
+import requests
+
+
+if __name__ == '__main__':
+    html = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(html.content.decode("utf-8"))))
+    print("\t- content: {}".format(html.content.decode("utf-8")))
