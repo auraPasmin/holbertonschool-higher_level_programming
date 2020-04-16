cdff6e9a3647ea1cb8f37e90a2ea9b06fb6eb874
auraPasmin <1288@holbertonschool.com>
2020-04-16 17:00:20 -0500

    add 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email address"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    res = requests.get(url)
+    if int(res.status_code) < 400:
+        print(r.text)
+    else:
+        print("Error code: {}".format(res.status_code))
