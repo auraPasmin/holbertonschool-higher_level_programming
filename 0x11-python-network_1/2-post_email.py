8dd701004210a69d37ff954816b6d4324bd411f8
auraPasmin <1288@holbertonschool.com>
2020-04-16 14:05:04 -0500

    add 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email"""
+
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": argv[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        email = response.read()
+    print(email.decode('utf-8'))
