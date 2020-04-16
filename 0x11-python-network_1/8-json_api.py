6fe3b972e56bacd61ff1ab13170aaea6ce9207de
auraPasmin <1288@holbertonschool.com>
2020-04-16 17:33:38 -0500

    add 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Write a Python script that takes in a letter and sends a POST"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        data = {'q': ''}
+    else:
+        data = {'q': '{}'.format(argv[1])}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    try:
+        user = r.json()
+        if user:
+            print("[{}] {}".format(user.get('id'), user.get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
