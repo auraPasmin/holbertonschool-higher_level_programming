cdb205371f874806289624b35d667355fd704d04
auraPasmin <1288@holbertonschool.com>
2020-04-16 15:36:55 -0500

    add 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Write a Python script that takes in a URL"""
+
+import urllib.error
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print("Error code:", error.code)
