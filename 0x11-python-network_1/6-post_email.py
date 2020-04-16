b60541a2266b39c1ea6327d66f3218ba1450fef7
auraPasmin <1288@holbertonschool.com>
2020-04-16 16:53:32 -0500

    add 6 permissions is ok

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" """
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    email = {'email': '{}'.format(argv[2])}
+    res = requests.post(argv[1], data=email)
+    print(res.text)
