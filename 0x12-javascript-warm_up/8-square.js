a61a83df0de350b7a7ef27ec281b91ea40db808a
auraPasmin <1288@holbertonschool.com>
2020-05-05 18:57:18 -0500

    add 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const arg = process.argv;
+
+if (!parseInt(arg[2])) {
+  console.log('Missing size');
+} else {
+  for (let y = parseInt(arg[2]); y > 0; y--) {
+    let size = '';
+    for (let x = parseInt(arg[2]); x > 0; x--) {
+      size += 'X';
+    }
+    console.log(size);
+  }
+}
