d33c989774daec936a325a6da194a0b6afd2f1c9
auraPasmin <1288@holbertonschool.com>
2020-05-05 19:36:37 -0500

    add 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// Write a script that searches the second biggest integer in the list of arguments.
+const len = process.argv.length;
+if (len <= 2) {
+  console.log(0);
+} else if (len === 3) {
+  console.log(0);
+} else {
+  secondBiggest();
+}
+
+function secondBiggest () {
+  const second = process.argv.sort(function (a, b) { return b - a; });
+  console.log(second[3]);
+}
