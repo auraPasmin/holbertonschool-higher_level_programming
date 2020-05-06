30994e87268d1acf700691eb02cb9ab57da28a8f
auraPasmin <1288@holbertonschool.com>
2020-05-05 20:21:35 -0500

    add 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorial (x) {
+  if (process.argv.length <= 2 || x === 0) {
+    return 1;
+  }
+  return (x * factorial(x - 1));
+}
+console.log(factorial(num));
