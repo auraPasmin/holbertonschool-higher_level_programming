1f55d79e68fbd78b38f63b937944f83dbef8e1e4
auraPasmin <1288@holbertonschool.com>
2020-05-11 16:57:27 -0500

    add 2 semi ok

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+/*
+Write a class Rectangle that defines a rectangle
+If w or h is equal to 0 or not a positive integer,
+create an empty object
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
