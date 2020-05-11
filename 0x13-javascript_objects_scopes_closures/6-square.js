b52bda0510b7e492152d4ecbbfe0e09065c48e40
auraPasmin <1288@holbertonschool.com>
2020-05-11 18:04:41 -0500

    add 6 semi ok

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+/*
+If c is undefined, use the character X
+*/
+class Square extends require('./5-square') {
+  charPrint (obj) {
+    if (obj === undefined) {
+      obj = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(obj.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
