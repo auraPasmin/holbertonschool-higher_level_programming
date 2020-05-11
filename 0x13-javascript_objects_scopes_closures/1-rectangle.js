2169c6353f4984d41abdaf98f29557f8ed1f873f
auraPasmin <1288@holbertonschool.com>
2020-05-11 16:17:36 -0500

    add 1 semi ok

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/*
+Write a class Rectangle that defines a rectangle
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
