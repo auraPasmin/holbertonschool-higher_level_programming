96bb9a063882f1a8b8a789b2a471adf802de191a
auraPasmin <1288@holbertonschool.com>
2020-05-11 17:23:41 -0500

    add 3 permissions ok

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+/*
+Write a class Rectangle that defines a rectangle
+If w or h is equal to 0 or not a positive integer,
+create an empty object
+Create an instance method called print()
+that prints the rectangle using the character X
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
