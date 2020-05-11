17d403b4eac0ea7be86e3d3e43311d39b7887246
auraPasmin <1288@holbertonschool.com>
2020-05-11 17:34:46 -0500

    add 4 semi ok

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,37 @@
 #!/usr/bin/node
+/*
+Write a class Rectangle that defines a rectangle
+If w or h is equal to 0 or not a positive integer,
+create an empty object
+Create an instance method called print()
+that prints the rectangle using the character X
+rotate() that exchanges the width and the height of the rectangle
+double() that multiples " " " " " " " "by 2
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
