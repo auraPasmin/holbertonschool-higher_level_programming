de8bcc6453f792a4cab1c166decf1ac8bcc6b3e3
auraPasmin <1288@holbertonschool.com>
2020-05-11 22:49:15 -0500

    add 10 semmi ok

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/*
+ hat converts a number from base 10 to
+ another base passed as argument:
+*/
+exports.converter = function (base) {
+  return function (number) {
+    return number.toString(base);
+  };
+};
