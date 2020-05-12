0af5b697e52027b23c964b590c2fec37edf6711d
auraPasmin <1288@holbertonschool.com>
2020-05-11 22:29:27 -0500

    add 7 semi ok

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/*
+that returns the number of occurrences in a list
+*/
+exports.nbOccurences = function (list, searchElement) {
+  let match = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      match++;
+    }
+  }
+  return match;
+};
