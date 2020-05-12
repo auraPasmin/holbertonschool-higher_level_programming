1f01969081b481c10506bbccc4e08736ea6b41f6
auraPasmin <1288@holbertonschool.com>
2020-05-11 22:37:29 -0500

    add 8 semi ok

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+/*
+  unction that returns the reversed version of a list:
+*/
+exports.esrever = function (list) {
+  const list2 = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    list2.push(list[i]);
+  }
+  return list2;
+};
