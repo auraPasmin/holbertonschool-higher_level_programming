bb884dd16c62c44e3a31b077711de831d2a3ce24
auraPasmin <1288@holbertonschool.com>
2020-05-13 11:16:23 -0500

    add 2 semi ok

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/*
+   string to write
+*/
+const request = require('request');
+request(process.argv[2], function (error, Response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + Response.statusCode);
+  }
+});
