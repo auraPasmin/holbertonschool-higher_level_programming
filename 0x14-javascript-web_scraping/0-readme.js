d0a69e572376d763fd8bd57248cb2778cb6b70e3
auraPasmin <1288@holbertonschool.com>
2020-05-12 19:52:13 -0500

    add 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/*
+  Write a script that reads and prints the content of a file.
+*/
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
