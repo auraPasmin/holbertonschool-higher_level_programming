622afe63d3867badfca59b26bd2010fc1de18a7b
auraPasmin <1288@holbertonschool.com>
2020-06-04 23:13:06 -0500

    fix 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/*
+   Star wars movie title
+*/
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
