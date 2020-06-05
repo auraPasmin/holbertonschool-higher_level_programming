e44a083189071d42c30d3ddd9c072b12042446b4
auraPasmin <1288@holbertonschool.com>
2020-06-04 23:22:51 -0500

    add 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+!/usr/bin/node
+/*
+  Loripsum
+*/
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (error, request, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    try {
+      fs.writeFileSync(process.argv[3], body, 'utf-8');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
