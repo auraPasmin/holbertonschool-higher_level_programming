17e4e942a579575b4f937195c89eb08fa599c2be
auraPasmin <1288@holbertonschool.com>
2020-06-04 23:26:28 -0500

    add 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+/*
+  How many completed?
+*/
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const dict = {};
+    JSON.parse(body).forEach(element => {
+      if (element.completed) {
+        if (dict[element.userId]) {
+          dict[element.userId] = dict[element.userId] + 1;
+        } else {
+          dict[element.userId] = 1;
+        }
+      }
+    });
+    console.log(dict);
+  }
+});
