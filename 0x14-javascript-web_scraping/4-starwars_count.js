0d18def0214f17e92774933de7d8da4d3e6ebb98
auraPasmin <1288@holbertonschool.com>
2020-06-04 23:14:49 -0500

    fix 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+/*
+  Star wars Wedge Antilles
+*/
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let cnt = 0;
+    JSON.parse(body).results.forEach(character => {
+      character.characters.forEach(link => {
+        if (link.endsWith('/18/')) {
+          cnt++;
+        }
+      });
+    });
+    console.log(cnt);
+  }
+});
